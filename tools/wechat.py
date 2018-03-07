4670d4551f4b66806ac91b140b79a66c5ee25835
Mickey Zhou <mickey.zhou@liulishuo.com>
2018-03-07 23:47:14 +0800

    add wechat

---
+++ tools/wechat.py
@@ -0,0 +1,40 @@
+# coding:utf8
+
+from wxpy import *
+import utils
+
+
+def start_group_bot(group_name):
+    bot = Bot(cache_path=True)
+    bot.enable_puid()
+    group = ensure_one(bot.groups(update=True).search(group_name))
+    print group
+
+    # The first matching function is registered at last,
+    # and only one registration function is matched.
+    register_print(bot)
+    register_repeat(bot, group)
+
+    welcome(group)
+
+
+def welcome(group):
+    pass
+    group.send(utils.decode_text_utf8("大家好,我是人工智障我叫缺西"))
+
+
+def register_print(bot):
+    @bot.register()
+    def just_print(msg):
+        print(msg)
+
+
+def register_repeat(bot, group):
+    @bot.register(group, TEXT)
+    def auto_reply(msg):
+        return "Return" + msg.text
+
+
+if __name__ == '__main__':
+    start_group_bot(utils.decode_text_utf8("缺西"))
+    embed()
